2a9045676472f00d87cef0f25467f25971e5f78b
codegenius2 <lucifer231220@gmail.com>
2023-10-21 15:20:26 +0900

    bitcoin recoverWallet() added

--- src/wallet_class/bitcoin/index.ts
+++ src/wallet_class/bitcoin/index.ts
@@ -45,6 +45,45 @@ class BitCoinWallet {
             mnemonic: mnemonic
         }
     }
+
+    importWallet = (mnemonic: string, network: string, derivedPath?: string): BtcAccount => {
+        let btcNetwork;
+    
+        switch(network) {
+            case BTC_MAINNET:
+                btcNetwork = bitcoin.networks.bitcoin;
+                break;
+            case BTC_REGTEST:
+                btcNetwork = bitcoin.networks.regtest;
+                break;
+            case BTC_TESTNET:
+                btcNetwork = bitcoin.networks.testnet;
+                break;
+            default:
+                btcNetwork = bitcoin.networks.bitcoin;
+                break;
+        }
+    
+        const path = derivedPath || BITCOIN_DEFAULT;
+    
+        const seed = bip39.mnemonicToSeedSync(mnemonic);
+        // const bip32 = BIP32Factory(ecc);
+        const root = bip32.fromSeed(seed, btcNetwork);
+    
+        const account = root.derivePath(path);
+        const node = account.derive(0);
+    
+        const address = bitcoin.payments.p2pkh({
+            pubkey: node.publicKey,
+            network: btcNetwork
+        }).address || '';
+    
+        return {
+            address: address,
+            privateKey: node.toWIF(),
+            mnemonic: mnemonic
+        }
+    }
 }
 
 export default BitCoinWallet
